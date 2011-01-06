9653dfa57c476a0cc6b3ae8bfa1df75412d96555
cvarin <cvarin@gmail.com>
2011-01-06 18:01:05 -0500

    Script qui permet d'effectuer une opération "git" sur tous les dépôt.

---
+++ all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+###############################################################################
+# For coloring bash echo, see http://www.faqs.org/docs/abs/HTML/colorizing.html
+red='\033[1;31m'
+reset_color='tput sgr0'
+
+###############################################################################
+echo
+reps=$(ls --ignore=list --ignore=*.sh)
+for i in $reps 
+  do 
+     echo -e "$red$i"; $reset_color
+     cd $i
+     git $@
+     cd ..
+     echo " "
+done
+
+###############################################################################
