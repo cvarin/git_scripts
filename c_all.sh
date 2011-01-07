115686f58742b4c3b1282263090ba16c76053692
charles <charles@supermicro3.(none)>
2011-01-07 12:33:35 -0500

    all.sh maintenant avec (c_all.sh) et sans couleurs (all.sh).

--- c_all.sh
+++ c_all.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 ###############################################################################
+# For coloring bash echo, see http://www.faqs.org/docs/abs/HTML/colorizing.html
+red='\033[1;31m'
+reset_color='tput sgr0'
+
+###############################################################################
 echo
 reps=$(ls --ignore=*.py --ignore=*.sh --ignore=*.list)
 for i in $reps 
   do 
-     echo "$i";
+     echo -e "$red$i"; $reset_color
      cd $i
      git $@
      cd ..
