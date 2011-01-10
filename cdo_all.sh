df43e90687ccbc6d8032d03c8b5ad5c0cf623abc
Charles Varin <cvarin@gmail.com>
2011-01-10 15:03:09 -0500

    Ajouté des scripts pour exécuter une opération générale (comme des scripts) sur l'ensemble des dossiers.

---
+++ cdo_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+###############################################################################
+# For coloring bash echo, see http://www.faqs.org/docs/abs/HTML/colorizing.html
+red='\033[1;31m'
+reset_color='tput sgr0'
+
+###############################################################################
+echo
+reps=$(ls --ignore=*.py --ignore=*.sh --ignore=*.list)
+for i in $reps 
+  do 
+     echo -e "$red$i"; $reset_color
+     cd $i
+     $@
+     cd ..
+     echo " "
+done
+
+###############################################################################
